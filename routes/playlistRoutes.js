1a486029fecc81555a1880948b5eb6882692861e
joji <rainbreeze2@naver.com>
2025-04-08 10:13:23 +0900

    playlist get API URL 익숙하게 수정

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -6,6 +6,6 @@ const playlistController = require('../controllers/playlistController');
 router.get('/:userId', playlistController.getPlaylistIds);
 
 // 플레이리스트에 해당하는 음악 ID 조회
-router.get('/music/:playlistId', playlistController.getPlaylistMusicIds);
+router.get('/playlistmusic/:playlistId', playlistController.getPlaylistMusicIds);
 
 module.exports = router;
