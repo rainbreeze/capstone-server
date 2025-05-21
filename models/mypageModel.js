63684c909f8c5b379c4a4ae2fcf762cc5fca3138
circlepaper <rmzj123456@gmail.com>
2025-05-21 16:16:21 +0900

    마이페이지 데이터 불러오기

--- models/mypageModel.js
+++ models/mypageModel.js
@@ -5,7 +5,7 @@ const db = require('../config/db'); // 예시: MySQL 연결 모듈
 const getUserById = (userId) => {
     return new Promise((resolve, reject) => {
         console.log('마이페이지 모델 작동 확인.')
-        const query = 'SELECT username, userId, email, createdAt FROM users WHERE id = ?';
+        const query = 'SELECT userName, userId, email, createdAt FROM users WHERE userId = ?';
         db.query(query, [userId], (err, results) => {
             if (err) return reject(err);
             resolve(results[0]);
