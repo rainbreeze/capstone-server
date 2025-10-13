5b97f0d621a012286b623ca5ee754dcfd5471786
joji <rainbreeze2@naver.com>
2025-10-13 17:31:16 +0900

    hello 테스팅

--- routes/genreApiRoutes.js
+++ routes/genreApiRoutes.js
@@ -10,6 +10,21 @@ dotenv.config();
 // BASE_URL: .env 파일에 정의된 FastAPI 서버 주소 사용
 const BASE_URL = process.env.FASTAPI_BASE_URL
 
+// GET /hello 라우트 추가
+router.get('/hello', async (req, res) => {
+    try {
+        console.log('hello 동작');
+
+        // FastAPI /hello GET 요청 보내기
+        const response = await axios.get(`${BASE_URL}/hello`);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('FastAPI /hello 호출 실패:', error.message);
+        res.status(500).json({ error: 'FastAPI 서버 호출에 실패했습니다.' });
+    }
+});
+
 // 새로 추가하는 /predict 라우트 (POST)
 router.post('/predict', async (req, res) => {
     try {
