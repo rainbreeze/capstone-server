fd524de11607094a3b17c9b52739843fe4b7aa10
hyun <hyunxx.seo@gmail.com>
2025-11-25 01:09:15 +0900

    에러 수정

--- models/gameStatModel.js
+++ models/gameStatModel.js
@@ -1,13 +1,14 @@
 const db = require('../config/db'); // DB 연결
 
-console.log("🟢 Loaded gameStatModel 최신버전");
+console.log("🟢 현재 로드된 게임 스탯 모델 경로:", __filename);
+
 
 
 //게임 스탯 저장
 const saveGameStats = async(userId, answer, steps, jumps, sprints, playTime, cleared) => {
     const [result] = await db.execute(
-        `INSERT INTO game_stats (userId, answer, steps, jumps, sprints, play_time, cleared) 
-         VALUES (?, ?, ?, ?, ?, ?, ?)`, [userId, answer, steps, jumps, sprints, playTime, cleared]
+        `INSERT INTO game_stats (userId, steps, sprints, play_time, jumps, cleared, answer) 
+         VALUES (?, ?, ?, ?, ?, ?, ?)`, [userId, steps, sprints, playTime, jumps, cleared, answer]
     );
     return result;
 };
