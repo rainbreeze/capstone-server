f7af87ff63e8181091a8757561c75ac902e76862
hyun <hyunxx.seo@gmail.com>
2025-12-03 02:21:34 +0900

    dot env 환경변수 수정 테스트

--- services/mlService.js
+++ services/mlService.js
@@ -1,12 +1,26 @@
 const axios = require('axios');
+
+const dotenv = require('dotenv');
+dotenv.config();
 const BASE_URL = process.env.FASTAPI_BASE_URL;
 
 exports.hello = async() => {
-    const response = await axios.get(`${BASE_URL}/hello`);
-    return response.data;
+
+    try {
+        const response = await axios.get(`${BASE_URL}/hello`);
+        return response.data;
+    } catch (error) {
+        console.log('/hello 호출 실패', error.message);
+    }
+
 };
 
 exports.predict = async(data) => {
-    const response = await axios.post(`${BASE_URL}/predict`, data);
-    return response.data;
+    try {
+        const response = await axios.post(`${BASE_URL}/predict`, data);
+        return response.data;
+    } catch (error) {
+        console.log('/hello 호출 실패', error.message);
+    }
+
 };
